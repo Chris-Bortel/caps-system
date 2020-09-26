ef719b4bdcd19caedf1f17657e8192be06b8621f
Chris Bortel <bortelcc19@gmail.com>
2020-09-26 15:17:23 -0700

    Repository setup is complete. The applications file structure is created and has the problem domain for each module commented in.

---
+++ driver.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+// Drivers Module
+
+// Monitor the system for events …
+
+// On the ‘pickup’ event …
+// Wait 1 second
+// // Log “DRIVER: picked up [ORDER_ID]” to the console.
+// // Emit an ‘in-transit’ event with the payload you received
+
+// Wait 3 seconds
+// //  --Log “delivered” to the console
+// // Emit a ‘delivered’ event with the same payload
+
---
+++ events.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Global Event Pool (shared by all modules)
+const Events = require('events');
+const events = new Events();
+
+module.exports = events;
